a938ee36715eff90a23662526200356320dba4a6
askari-fahlyani <askari.fahlyani@gmail.com>
2024-09-26 18:05:26 +0200

    Updates

--- src/hooks/useAppContext.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import AppContext from "../Components/context/AppContext";
-
-const useAppContext = () => {
-  return useContext(AppContext);
-};
-
-export default useAppContext;
